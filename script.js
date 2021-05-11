b1f80445e925ba1729ce4456f4bc98f3739198ae
Соломон Захаров <solomon7and@gmail.com>
2021-05-11 19:25:30 +0500

    js поиск уровня, скрытие меню и показ карт соответств лэвлу

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // 1.Присвоение уровня сложности.
-// 1.1. Обозначить переменные - уровней.
+// 1.1. Обозначить переменные.
 
 let levelEasy = document.querySelector('.nav__easy');
 let levelNormal = document.querySelector('.nav__normal');
 let levelHard = document.querySelector('.nav__hard');
+let button = document.querySelector('.button');
+let easy = document.querySelector('.mark');
+let normal = document.querySelector('.mark');
+let hard = document.querySelector('.mark');
+let menu = document.querySelector('.menu')
+let game = document.querySelector('.game')
 
 // 1.2. При клике на уровень сложности назначается класс mark.
 
@@ -29,5 +35,38 @@ levelEasy.addEventListener('click', markEasy);
 levelNormal.addEventListener('click', markNormal);
 levelHard.addEventListener('click', markHard);
 
-// 2. выбранный уровень попадает в кнопку - начать игру.
+// 2. При нажатии кнопки "Начать игру" скрывается меню.
+// Вместе с этим идет поиск уровня и запускает функцию с соответствующим уровнем.
 
+function hiddenMenu () {
+  menu.classList.add('hidden')
+}
+
+function discoverLevel() {
+  if (easy = true) {
+    showEasy ()
+  } if (normal = true) {
+    markHard ()
+  } if (hard = true) {
+    markHard ()
+    hiddenMenu ()
+  } else {
+    console.error(onerror)
+  }
+}
+
+function showEasy () {
+  game.innerHTML = `<div class="card">
+  <img class="card__back" src="images/Карта%20BACK.png" alt="задняя сторона">
+  <img class="card__front" src="images/Карта%20Game%20Over.png" alt="Игра окончена">
+</div>
+
+<div class="card">
+  <img class="card__back" src="images/Карта%20BACK.png" alt="задняя сторона">
+  <img class="card__front" src="images/Карта%20BUG.png" alt="Победа!">
+</div>`
+}
+
+button.addEventListener('click', discoverLevel );
+
+// Нажимая на кнопку "Начать игру", скрывается меню и выдается нужное количество карт карт
