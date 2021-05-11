3c5954ba08d9a7578662dfd3b9cd621c1f221d49
Соломон Захаров <solomon7and@gmail.com>
2021-05-11 17:12:19 +0500

    При клике на уровень сложности назначается класс mark.

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 // 1.Присвоение уровня сложности.
 // 1.1. Обозначить переменные - уровней.
 
-let levelEasy = document.querySelectorAll('.nav__easy');
-let levelNormal = document.querySelectorAll('.nav__normal');
-let levelHard = document.querySelectorAll('.nav__hard');
+let levelEasy = document.querySelector('.nav__easy');
+let levelNormal = document.querySelector('.nav__normal');
+let levelHard = document.querySelector('.nav__hard');
 
 // 1.2. При клике на уровень сложности назначается класс mark.
 
-// let markEasy = () => levelEasy.classList.toggle('mark');
-// let markNormal = () => levelNormal.classList.toggle('mark');
-// let markHard = () => levelHard.classList.toggle('mark');
+function markEasy () {
+  levelEasy.classList.add('mark');
+  levelNormal.classList.remove('mark');
+  levelHard.classList.remove('mark');
+}
 
-function markEasy () { levelEasy.classList.toggle('mark'); }
-function markNormal () { levelNormal.classList.toggle('mark'); }
-function markHard () { levelHard.classList.toggle('mark'); }
+function markNormal () {
+  levelEasy.classList.remove('mark');
+  levelNormal.classList.add('mark');
+  levelHard.classList.remove('mark');
+}
+
+function markHard () {
+  levelEasy.classList.remove('mark');
+  levelNormal.classList.remove('mark');
+  levelHard.classList.add('mark');
+}
 
 levelEasy.addEventListener('click', markEasy);
 levelNormal.addEventListener('click', markNormal);
 levelHard.addEventListener('click', markHard);
-
 
 // 2. выбранный уровень попадает в кнопку - начать игру.
 
